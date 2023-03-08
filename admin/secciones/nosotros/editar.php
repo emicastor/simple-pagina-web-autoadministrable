867d34ec0ad64bff91c9e253cff57e3c9d195a0e
Emiliano Castor <castoremi12@gmail.com>
2023-03-08 19:36:09 -0300

    Agregar archivos a la sección nosotros

---
+++ admin/secciones/nosotros/editar.php
@@ -0,0 +1,3 @@
+<?php include '../../templates/header.php'; ?>
+Editar nosotros
+<?php include '../../templates/footer.php'; ?>
---
+++ admin/secciones/nosotros/index.php
@@ -0,0 +1,3 @@
+<?php include '../../templates/header.php'; ?>
+Listar nosotros
+<?php include '../../templates/footer.php'; ?>
