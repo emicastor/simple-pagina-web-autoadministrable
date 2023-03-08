82fb8d3c4c279b6f60d64f281614ce7b2e5a7bf1
Emiliano Castor <castoremi12@gmail.com>
2023-03-08 17:59:24 -0300

    Agregar index.php de la carpeta admin

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php include 'templates/header.php'; ?>
+
+
+<div class="p-2 mt-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-2 py-md-5">
+      <h1 class="display-5 fw-bold">Bienvenido a su panel administrativo</h1>
+      <p class="col-md-8 fs-4">Desde este panel usted podrá administrar toda su página web haciendo uso de la barra de navegación situada en la parte superior de su pantalla. Podrá modificar todas las secciones de la misma, ya sean títulos, subtítulos, imágenes, textos, etc.</p>
+      <p class="col-md-8 fs-6"><strong class="text-decoration-underline">Aclaración</strong>: Si usted ingresó desde un <strong>dispositivo móvil</strong> , podrá encontrar la barra de navegación tocando en el ícono de las <strong>3 rayitas</strong> situado en la parte superior derecha de su pantalla.</p>
+    </div>
+  </div>
+
+
+<?php include 'templates/footer.php'; ?>
+    
