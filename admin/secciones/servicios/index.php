f12005f53195d3772a08aeb84b0ae49c3fde6bee
Emiliano Castor <castoremi12@gmail.com>
2023-03-09 02:16:32 -0300

    Mostrar registros en tabla

--- admin/secciones/servicios/index.php
+++ admin/secciones/servicios/index.php
@@ -1,3 +1,35 @@
 <?php include '../../templates/header.php'; ?>
-Listar servicios
+
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear" role="button">Agregar registro</a>
+    </div>
+    <div class="card-body">
+
+        <div class="table-responsive-sm">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Ícono</th>
+                        <th scope="col">Título</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">1</td>
+                        <td>fa-book</td>
+                        <td>Tutorías</td>
+                        <td>Servicios de tutorías para programación</td>
+                        <td>Editar | Eliminar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
 <?php include '../../templates/footer.php'; ?>
