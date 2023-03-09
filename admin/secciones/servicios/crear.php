f943ebac7eaa86b981944f7da5b785e47466d6dc
Emiliano Castor <castoremi12@gmail.com>
2023-03-09 02:53:44 -0300

    Agregar formulario para crear un servicio

--- admin/secciones/servicios/crear.php
+++ admin/secciones/servicios/crear.php
@@ -1,3 +1,39 @@
 <?php include '../../templates/header.php'; ?>
-Crear servicios
+
+<!-- <div class="row"> -->
+<!-- <div class="col-12 col-md-12 col-lg-8 mx-auto"> -->
+<div class="card shadow">
+    <div class="card-header py-3 fs-5 fw-semibold text-center">
+        Crear un servicio
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-8 mx-auto">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="icono" class="form-label fw-semibold">Ícono</label>
+                        <input type="text" class="form-control shadow-sm" name="icono" id="icono" aria-describedby="helpId" placeholder="">
+                    </div>
+                    <div class="mb-3">
+                        <label for="titulo" class="form-label fw-semibold">Título</label>
+                        <input type="text" class="form-control shadow-sm" name="titulo" id="titulo" aria-describedby="helpId" placeholder="">
+                    </div>
+                    <div class="mb-4">
+                        <label for="descripcion" class="form-label fw-semibold">Descripción</label>
+                        <input type="text" class="form-control shadow-sm" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary fw-semibold px-md-4 py-md-2 me-2">Agregar</button>
+                    <a name="" id="" class="btn btn-outline-secondary fw-semibold px-md-4 py-md-2" href="<?= $url_base; ?>/secciones/servicios" role="button">Cancelar</a>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer text-muted">
+    </div>
+
+</div>
+<!-- </div> -->
+<!-- </div> -->
+
 <?php include '../../templates/footer.php'; ?>
