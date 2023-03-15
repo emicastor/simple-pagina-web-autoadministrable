f0862b47a11931a5121b89784178efa433c448c0
Emiliano Castor <castoremi12@gmail.com>
2023-03-15 12:14:54 -0300

    Agregar la librería SweetAlert2

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -10,7 +10,10 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <!-- Jquery Datatable JS -->
     <script charset="utf8" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-   
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="http://localhost/simple-pagina-web-autoadministrable/assets/js/admin.js"></script>
+
     </body>
 
     </html>
