c54208e91c5a944e0cb0807603e29bb3ccc1bac1
Emiliano Castor <castoremi12@gmail.com>
2023-03-20 18:57:08 -0300

    Agregar nav offcanvas

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['usuario'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" class="h-100">
 
 <head>
     <meta charset="UTF-8">
@@ -25,54 +25,57 @@ if (!isset($_SESSION['usuario'])) {
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <!-- Jquery Datatable JS -->
     <script charset="utf8" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- CSS custom| -->
+    <link rel="stylesheet" href="../../assets/css/style.css">
 
-    <style>
-        .img {
-            max-inline-size: 100%;
-            block-size: auto;
-            aspect-ratio: 16/9;
-            object-fit: cover;
-            object-position: center;
-        }
-    </style>
 </head>
 
-<body class="bg-light">
+<body class="bg-light d-flex flex-column h-100">
 
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-white py-4 shadow-sm">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white py-4 shadow-sm" aria-label="Offcanvas navbar large">
             <div class="container">
-                <a class="navbar-brand" href="<?= $url_base; ?>">Administrador</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="<?= $url_base; ?>">Panel</a>
+                <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= $url_base; ?>secciones/servicios/" aria-current="page">💼 Servicios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= $url_base; ?>secciones/portafolio">🗂️ Portafolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= $url_base; ?>secciones/nosotros">👥 Nosotros</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= $url_base; ?>secciones/equipo">🤝 Equipo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= $url_base; ?>secciones/usuarios/">🪪 Usuarios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= $url_base; ?>secciones/configuraciones">⚙️ Configuraciones</a>
-                        </li>
-                    </ul>
-                    <div class="pt-4 pt-lg-0">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="btn btn-outline-secondary d-block" href="<?= $url_base; ?>cerrar" role="button">🔑 Cerrar sesión</a>
-                            </li>
-                        </ul>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+                    <div class="container px-3">
+                        <div class="offcanvas-header border-bottom">
+                            <a class="navbar-brand" href="<?= $url_base; ?>">Panel administrativo</a>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= $url_base; ?>secciones/servicios/" aria-current="page">💼 Servicios</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= $url_base; ?>secciones/portafolio">🗂️ Portafolio</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= $url_base; ?>secciones/nosotros">👥 Nosotros</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= $url_base; ?>secciones/equipo">🤝 Equipo</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= $url_base; ?>secciones/usuarios/">🪪 Usuarios</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= $url_base; ?>secciones/configuraciones">⚙️ Configuraciones</a>
+                                </li>
+                            </ul>
+                            <div class="pt-4 pt-lg-0">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="btn btn-outline-secondary d-block" href="<?= $url_base; ?>cerrar" role="button">🔑 Cerrar sesión</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
