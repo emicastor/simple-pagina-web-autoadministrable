53a108473be7772d802fbafe2d78dbfc052ca559
Emiliano Castor <castoremi12@gmail.com>
2023-03-20 18:42:59 -0300

    Cambiar header con offcanvas

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,11 @@
 <?php include 'templates/header.php'; ?>
 
 
-<div class="p-2 mt-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-2 py-md-5">
-      <h1 class="display-5 fw-bold">Bienvenido a su panel administrativo</h1>
-      <p class="col-md-8 fs-4">Desde este panel usted podrá administrar toda su página web haciendo uso de la barra de navegación situada en la parte superior de su pantalla. Podrá modificar todas las secciones de la misma, ya sean títulos, subtítulos, imágenes, textos, etc.</p>
-      <p class="col-md-8 fs-6"><strong class="text-decoration-underline">Aclaración</strong>: Si usted ingresó desde un <strong>dispositivo móvil</strong> , podrá encontrar la barra de navegación tocando en el ícono de las <strong>3 rayitas</strong> situado en la parte superior derecha de su pantalla.</p>
+<div class="p-3 p-sm-5 mb-4 bg-white rounded-3 shadow">
+    <div class="container-fluid">
+      <h1 class="display-6 fw-bold">Bienvenido a su panel administrativo 📝</h1>
+      <p class="col-md-10 col-xl-8 fs-4 mb-5">Desde este panel usted podrá administrar toda su página web haciendo uso de la barra de navegación situada en la parte superior de su pantalla. Podrá modificar todas las secciones de la misma, ya sean títulos, subtítulos, imágenes, textos, etc.</p>
+      <p class="border rounded-3 p-3 col-md-10 col-xl-8 fs-6"><strong class="text-decoration-underline">Aclaración</strong>: Si usted ingresó desde un📱<strong>dispositivo móvil</strong>, podrá encontrar la barra de navegación tocando en el ícono de las <strong>3 rayitas</strong> situado en la parte superior derecha de su pantalla.</p>
     </div>
   </div>
 
