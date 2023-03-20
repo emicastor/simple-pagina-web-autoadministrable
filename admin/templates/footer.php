d488c7bf5f70474d523aaa15f2992fdbf2d0e245
Emiliano Castor <castoremi12@gmail.com>
2023-03-20 18:57:31 -0300

    Agregar footer a las secciones dentro de admin

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -1,18 +1,27 @@
     </main>
-
-    <footer>
-
+    <div class="mb-5 pb-4"></div>
+    <footer class="mt-auto bg-white shadow-sm">
+        <div class="container py-4 text-center">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <a href="https://emicastor.com.ar/agencia" class="d-flex flex-column align-items-center text-decoration-none logo-footer">
+                        <span class="text-dark">Emiliano Castor</span>
+                        <span class="px-1 bg-dark text-white fw-semibold py-1" style="font-size: 10px;">DESARROLLADOR WEB</span>
+                    </a>
+                </div>
+                <div class="col-md-5 text-lg-end">
+                    <span class="text-muted">Por consultas o problemas: <a href="mailto:contacto@emicastor.com.ar">contacto@emicastor.com.ar</a></span>
+                </div>
+            </div>
+        </div>
     </footer>
-
     <!-- JS Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <!-- Jquery Datatable JS -->
     <script charset="utf8" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <!-- SweetAlert2 -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="http://localhost/simple-pagina-web-autoadministrable/assets/js/admin.js"></script>
+    <script src="<?= $url_base;?>assets/js/admin.js"></script>
 
     </body>
 
